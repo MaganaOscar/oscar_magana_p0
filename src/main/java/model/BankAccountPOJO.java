cbc90ea4200b2986d79287357b669bffd16a2b78
MaganaOscar <oscmag13@gmail.com>
2022-05-10 22:12:52 -0700

    bank account and user model pojos created

--- src/main/java/model/BankAccountPOJO.java
+++ src/main/java/model/BankAccountPOJO.java
@@ -5,4 +5,45 @@ public class BankAccountPOJO {
 	private char accountType;
 	private double balance;
 	
+	public BankAccountPOJO() {
+		super();
+	}
+	
+	public BankAccountPOJO(int accountID, char accountType, double balance) {
+		super();
+		this.accountID = accountID;
+		this.accountType = accountType;
+		this.balance = balance;
+	}
+
+	public int getAccountID() {
+		return accountID;
+	}
+
+	public void setAccountID(int accountID) {
+		this.accountID = accountID;
+	}
+
+	public char getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(char accountType) {
+		this.accountType = accountType;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return "BankAccountPOJO [accountID=" + accountID + ", accountType=" + accountType + ", balance=" + balance
+				+ "]";
+	}
+	
 }
--- src/main/java/model/UserPOJO.java
+++ src/main/java/model/UserPOJO.java
@@ -1,5 +1,50 @@
 package model;
 
 public class UserPOJO {
+	private int userID;
+	private String username;
+	private String password;
+	
+	public UserPOJO() {
+		super();
+	}
+	
+	public UserPOJO(int userID, String username, String password) {
+		super();
+		this.userID = userID;
+		this.username = username;
+		this.password = password;
+	}
 
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "UserPOJO [userID=" + userID + ", username=" + username + ", password=" + password + "]";
+	}
+	
+	
+	
 }
